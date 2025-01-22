393cffe97959c9129861ad647267ece10a6057d6
ddm479 <ehdtjq5247@gmail.com>
2025-01-22 21:53:45 +0900

    13-useReducer_strict_action_types action객체 타입 지정할 때 ?(optional)사용하지 않는걸 권장

--- typescript/codevolution/react-ts/src/components/state/12~13-useReducer_hook/Counter.tsx
+++ typescript/codevolution/react-ts/src/components/state/12~13-useReducer_hook/Counter.tsx
@@ -4,12 +4,26 @@ type CounterState = {
     count: number
 }
 
-// action 타입 지정
-type CounterAction = {
-    type: 'increment' | 'decrement' | 'reset' // 템플릿 리터럴로 구체적으로 지정
+// 1. action 타입 지정
+// type CounterAction = {
+//     type: 'increment' | 'decrement' | 'reset' // 템플릿 리터럴로 구체적으로 지정
+//     payload?: number // payload가 undefined값을 가질 수 있어 action.payload에서 오류발생 -> Action 객체 타입지정을 2개로 분류
+// }
+
+// Update action 타입 지정 , 2. 아래방식을 권장한다.
+type UpdateAction = {
+    type: 'increment' | 'decrement'
     payload: number
 }
-const initialState = { count:0 }
+
+// Reset Action 타입 지정
+type ResetAction = {
+    type: 'reset'
+}
+
+type CounterAction = UpdateAction | ResetAction
+
+const initialState = { count: 0 }
 
 // state를 action.type에 따라서 업데이트하는 함수
 function reducer(state: CounterState, action: CounterAction) {
@@ -18,6 +32,8 @@ function reducer(state: CounterState, action: CounterAction) {
             return { count: state.count + action.payload }
         case 'decrement':
             return { count: state.count - action.payload }
+        case 'reset':
+            return initialState
         default:
             return state
     }
@@ -29,10 +45,10 @@ export const Counter = () => {
     return (
         <>
             Count: {state.count}
-            <button onClick={() => dispatch({ type: 'increment', payload: 10})}>
+            <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>
                 Increment 10
             </button>
-            <button onClick={() => dispatch({ type: 'decrement', payload: 10})}>
+            <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
                 Decrement 10
             </button>
         </>
