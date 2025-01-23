9c02485e9815e6787726dafde0fc7ff121c0eeaf
ddm479 <ehdtjq5247@gmail.com>
2025-01-23 15:24:59 +0900

    14-useContext_Hook

---
+++ typescript/codevolution/react-ts/src/app/App14.tsx
@@ -0,0 +1,12 @@
+import { ThemeContextProvider } from "../components/context-14~15/ThemeContext";
+import { Box } from '../components/context-14~15/Box'
+
+function App14() {
+    return <div>
+        <ThemeContextProvider>
+            <Box />
+        </ThemeContextProvider>
+    </div>
+}
+
+export default App14
